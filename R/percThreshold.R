61531964d3818f8c431dc699a6acd399f30896b8
mklau <matthewklau@fas.harvard.edu>
2019-05-24 11:37:59 +0800

    updates percThreshold.R and Rd

--- R/percThreshold.R
+++ R/percThreshold.R
@@ -1,9 +1,7 @@
 #' Calculates the percolation threshold.
 #' 
-#' Calculates the percolation threshold to determine the minimal network using
-#' the methods of Araujo et al. 2011.
-#' 
-#' This function is a dependency for the co.net and CoNetwork functions.
+#' Determine the minimal network using the percolation threshold
+#' method from \insertCite{Araujo2011}{conetto}.
 #' 
 #' @param x A network in matrix form.
 #' @param step.size The increment to use in determining the threshold.
@@ -14,33 +12,13 @@
 #' @keywords ~kwd1 ~kwd2
 #' @examples
 #' 
-#' ##---- Should be DIRECTLY executable !! ----
-#' ##-- ==>  Define data, use random,
-#' ##--	or do  help(data=index)  for the standard data sets.
-#' 
-#' ## The function is currently defined as
-#' function (x = "network matrix", step.size = 0.01) 
-#' {
-#'     no.c <- no.clusters(graph.adjacency(x, weighted = TRUE))
-#'     step <- 1
-#'     while (no.c == 1) {
-#'         x[x <= (step * step.size)] <- 0
-#'         no.c <- no.clusters(graph.adjacency(x, weighted = TRUE))
-#'         step <- step + 1
-#'     }
-#'     out <- list(threshold = ((step - 1) * step.size), isolated.nodes = colnames(x)[clusters(graph.adjacency(x, 
-#'         weighted = TRUE))$membership == 2])
-#'     return(out)
-#'   }
-#' 
 #' @export percThreshold
-percThreshold <-
-function(x = 'network matrix',step.size = 0.01){
+percThreshold <-function(x = 'network matrix',step.size = 0.01){
   no.c <- no.clusters(graph.adjacency(x,weighted = TRUE))
   step <- 1
   while (no.c == 1){
-    x[x <= (step*step.size)] <- 0
-    no.c <- no.clusters(graph.adjacency(x,weighted = TRUE))
+    x[x <= (step * step.size)] <- 0
+    no.c <- no.clusters(graph.adjacency(x, weighted = TRUE))
     step <- step + 1
   }
   out <- list(threshold = ((step - 1) * step.size), 
